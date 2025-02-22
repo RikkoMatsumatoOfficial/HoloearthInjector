69722c128ac4afff298d720e1e38b4e4cf289e29
Rikko Matsumato <kuttimsx@hotmail.com>
2025-02-22 06:21:36 +0200

    Add files via upload

---
+++ HoloInj.py
@@ -0,0 +1,17 @@
+import pymem as p
+import pymem.process as xz
+from os import _exit as exitfunction
+def Get_HoloearthProcess():
+    try:
+        return p.Pymem("Holoearth.exe")
+    except:
+        print("Not Founded Holoearth Process!!!")
+        exitfunction(3312)
+        
+
+def Get_ProcessHandle():
+    return Get_HoloearthProcess().process_handle
+
+def Inject(path_file):
+    convertbytes = bytes(path_file, "utf-8")
+    xz.inject_dll(Get_ProcessHandle(), convertbytes)
