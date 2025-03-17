598fb4b3fa63a9371750499cc2ff1ee895b1e18a
Rikko Matsumato <kuttimsx@hotmail.com>
2025-03-17 09:13:12 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,11 @@
 import PyBass.bass as b
 import HoloInj as holoearth_injector
 import ctypes as ct
-import pathlib as z
+import pathlib as st
 import os
 currentdevice = int(-1)
 def Main():
-    if(z.Path("BABEJUMADIBA.mp3").is_file()):
+    if(st.Path("BABEJUMADIBA.mp3").is_file()):
         if(b.BASS_INIT(device=currentdevice, freq=48000, flags=0, win=0, dsguid=0)):
             b.BASS_START()
             boom = b.BASS_StreamCreateFile(mem=0, filename=bytes("BABEJUMADIBA.mp3", "utf-8"), offset=0, length=0, flags=0x4)
@@ -18,7 +18,7 @@ def Main():
         pathf = input("Write Path to You're DLL File for Game Holoearth: ")
         holoearth_injector.Inject(path_file=pathf)
     else:
-        print("Not Founded BABEJUMADIBA.mp3")
+        print("Not Founded BABEJUMADIBA.mp3(Music File)")
         os._exit(3321)
 
 if __name__ == "__main__":
